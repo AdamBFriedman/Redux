c99b07f94f792c34006b7636ebc15bb7ad23c421
Adam Friedman <adam@blok.party>
2020-08-04 16:19:05 -0700

    Added store methods to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp';
+import store from './store';
+import * as actions from './actionTypes';
+
+store.subscribe(() => {
+	console.log('Store changed!', store.getState());
+});
+
+store.dispatch({
+	type: actions.BUG_ADDED,
+	payload: {
+		description: 'Bug 1',
+	},
+});
+
+store.dispatch({
+	type: actions.BUG_REMOVED,
+	payload: {
+		id: 1,
+	},
+});
